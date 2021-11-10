bcb93599fbc51999d952f73f7084cb1128710084
guikevin <guikevin23@gmail.com>
2021-11-10 19:29:15 +0800

    spinCube3Drotate

---
+++ 3DRotate/Assets/Script/spinCube.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spinCube : MonoBehaviour
+
+{
+
+    public Vector3 rotateAmount;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotateAmount);
+ 
+    }
+}
